360eb6c33b15e403192fc27973fce5ef81bbc6e6
David Jimenez <djimenez92@outlook.es>
2020-02-29 00:38:57 +0100

    Create XmlFactory close #6

---
+++ FileManager.DataAccess.Data/XmlFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileManager.Common.Layer;
+
+namespace FileManager.DataAccess.Data
+{
+	class XmlFactory : IFileFactory
+	{
+		public Student Add(Student studen)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Delete(Student student)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Student> Get()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Student Update(Student student)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
