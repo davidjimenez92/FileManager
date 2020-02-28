57e947b449acc384d9a0cd82e16d11172c1379a8
David Jimenez <djimenez92@outlook.es>
2020-02-29 00:25:08 +0100

    Create main form close #2

--- FileManager.Presentation.WinSite/Form1.Designer.cs
+++ FileManager.Presentation.WinSite/Form1.Designer.cs
@@ -28,13 +28,133 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.lblStudent = new System.Windows.Forms.Label();
+			this.lblName = new System.Windows.Forms.Label();
+			this.lblSurname = new System.Windows.Forms.Label();
+			this.lblDateOfBirth = new System.Windows.Forms.Label();
+			this.lblType = new System.Windows.Forms.Label();
+			this.tbId = new System.Windows.Forms.TextBox();
+			this.tbSurname = new System.Windows.Forms.TextBox();
+			this.tbName = new System.Windows.Forms.TextBox();
+			this.cbType = new System.Windows.Forms.ComboBox();
+			this.dpDate = new System.Windows.Forms.DateTimePicker();
+			this.SuspendLayout();
+			// 
+			// lblStudent
+			// 
+			this.lblStudent.AutoSize = true;
+			this.lblStudent.Location = new System.Drawing.Point(12, 97);
+			this.lblStudent.Name = "lblStudent";
+			this.lblStudent.Size = new System.Drawing.Size(76, 17);
+			this.lblStudent.TabIndex = 0;
+			this.lblStudent.Text = "Student Id:";
+			// 
+			// lblName
+			// 
+			this.lblName.AutoSize = true;
+			this.lblName.Location = new System.Drawing.Point(12, 152);
+			this.lblName.Name = "lblName";
+			this.lblName.Size = new System.Drawing.Size(45, 17);
+			this.lblName.TabIndex = 1;
+			this.lblName.Text = "Name";
+			// 
+			// lblSurname
+			// 
+			this.lblSurname.AutoSize = true;
+			this.lblSurname.Location = new System.Drawing.Point(12, 207);
+			this.lblSurname.Name = "lblSurname";
+			this.lblSurname.Size = new System.Drawing.Size(69, 17);
+			this.lblSurname.TabIndex = 2;
+			this.lblSurname.Text = "Surname:";
+			// 
+			// lblDateOfBirth
+			// 
+			this.lblDateOfBirth.AutoSize = true;
+			this.lblDateOfBirth.Location = new System.Drawing.Point(12, 263);
+			this.lblDateOfBirth.Name = "lblDateOfBirth";
+			this.lblDateOfBirth.Size = new System.Drawing.Size(90, 17);
+			this.lblDateOfBirth.TabIndex = 3;
+			this.lblDateOfBirth.Text = "Date of birth:";
+			// 
+			// lblType
+			// 
+			this.lblType.AutoSize = true;
+			this.lblType.Location = new System.Drawing.Point(404, 97);
+			this.lblType.Name = "lblType";
+			this.lblType.Size = new System.Drawing.Size(86, 17);
+			this.lblType.TabIndex = 4;
+			this.lblType.Text = "Output type:";
+			// 
+			// tbId
+			// 
+			this.tbId.Location = new System.Drawing.Point(113, 94);
+			this.tbId.Name = "tbId";
+			this.tbId.Size = new System.Drawing.Size(270, 22);
+			this.tbId.TabIndex = 5;
+			// 
+			// tbSurname
+			// 
+			this.tbSurname.Location = new System.Drawing.Point(113, 204);
+			this.tbSurname.Name = "tbSurname";
+			this.tbSurname.Size = new System.Drawing.Size(270, 22);
+			this.tbSurname.TabIndex = 6;
+			// 
+			// tbName
+			// 
+			this.tbName.Location = new System.Drawing.Point(113, 149);
+			this.tbName.Name = "tbName";
+			this.tbName.Size = new System.Drawing.Size(270, 22);
+			this.tbName.TabIndex = 7;
+			// 
+			// cbType
+			// 
+			this.cbType.FormattingEnabled = true;
+			this.cbType.Location = new System.Drawing.Point(496, 94);
+			this.cbType.Name = "cbType";
+			this.cbType.Size = new System.Drawing.Size(270, 24);
+			this.cbType.TabIndex = 8;
+			// 
+			// dpDate
+			// 
+			this.dpDate.Location = new System.Drawing.Point(113, 258);
+			this.dpDate.Name = "dpDate";
+			this.dpDate.Size = new System.Drawing.Size(270, 22);
+			this.dpDate.TabIndex = 9;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.dpDate);
+			this.Controls.Add(this.cbType);
+			this.Controls.Add(this.tbName);
+			this.Controls.Add(this.tbSurname);
+			this.Controls.Add(this.tbId);
+			this.Controls.Add(this.lblType);
+			this.Controls.Add(this.lblDateOfBirth);
+			this.Controls.Add(this.lblSurname);
+			this.Controls.Add(this.lblName);
+			this.Controls.Add(this.lblStudent);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label lblStudent;
+		private System.Windows.Forms.Label lblName;
+		private System.Windows.Forms.Label lblSurname;
+		private System.Windows.Forms.Label lblDateOfBirth;
+		private System.Windows.Forms.Label lblType;
+		private System.Windows.Forms.TextBox tbId;
+		private System.Windows.Forms.TextBox tbSurname;
+		private System.Windows.Forms.TextBox tbName;
+		private System.Windows.Forms.ComboBox cbType;
+		private System.Windows.Forms.DateTimePicker dpDate;
 	}
 }
 
