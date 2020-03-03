6ad38fdcef53cc52c9083aef14d49fd530c593cd
David Jimenez <djimenez92@outlook.es>
2020-03-03 19:02:33 +0100

    Implements reflection close #18

--- FileManager.DataAccess.Data/FileFactory.cs
+++ FileManager.DataAccess.Data/FileFactory.cs
@@ -1,23 +1,29 @@
 ﻿
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
 using FileManager.Common.Layer;
 
 namespace FileManager.DataAccess.Data
 {
 	public class FileFactory : IAbstractFactory
 	{
+		private readonly string path = ConfigurationManager.AppSettings.Get("fileConfig");
+
 		public VuelingFile Create(EnumTypes type)
 		{
-			switch (type)
-			{
-				case EnumTypes.XML:
-					return new XmlFile();
-				case EnumTypes.TXT:
-					return new TxtFile();
-				case EnumTypes.JSON:
-					return new JsonFile();
-				default:
-					return null;
-			}
+			var myAssembly = Assembly.GetExecutingAssembly();
+			XElement root = XElement.Load(path);
+			IEnumerable<XElement> repository =
+				from element in root.Elements("Type")
+				where (string)element.Attribute("Id") == type.ToString()
+				select element;
+			var fileType = repository.First().Element("class").Value;
+			Type newFileManager = myAssembly.GetType(fileType);
+			return Activator.CreateInstance(newFileManager) as VuelingFile;
 		}
 
 	}
